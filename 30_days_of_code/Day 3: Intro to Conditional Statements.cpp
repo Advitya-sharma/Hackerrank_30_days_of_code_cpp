e61367a5acbe25ffb54c78bc8fcf6a52bca3263f
Advitya sharma <54709416+Advitya-sharma@users.noreply.github.com>
2019-10-07 23:46:38 +0530

    Create Day 3: Intro to Conditional Statements.cpp

---
+++ 30_days_of_code/Day 3: Intro to Conditional Statements.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+    if(N%2==0 && (N>=2 && N<=5)){
+        cout<<"Not Weird";
+    }
+    else if(N%2==0&& N>=6 && N<=20){
+        cout<<"Weird";
+    }
+    else if(N>20 && N%2==0){
+        cout<<"Not Weird";
+    }
+    else if(N%2!=0){
+        cout<<"Weird";
+    }
+
+    return 0;
+}
